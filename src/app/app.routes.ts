0b379f1296e96ef17ff76951fce17f44b721ed03
chandrabose27 <102638656+chandrabose27@users.noreply.github.com>
2025-06-09 23:56:47 +0530

    Day 1: Set up routing two urls are working fine

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,29 +5,29 @@ import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 
 export const routes: Routes = [
-    {
-    path: '',
-    loadComponent: () =>
-      import('./pages/home/home.component').then((m) => m.HomeComponent)
-  },
-  {
-    path: 'about',
-    loadComponent: () =>
-      import('./pages/about/about.component').then((m) => m.AboutComponent)
-  },
-  {
-    path: 'contact',
-    loadComponent: () =>
-      import('./pages/contact/contact.component').then((m) => m.ContactComponent)
-  },
-  {
-    path: '**',
-    redirectTo: ''
-  }
-//   { path: '', component: HomeComponent },
-//   { path: 'about', component: AboutComponent },
-//   { path: 'contact', component: ContactComponent },
-//   { path: '**', redirectTo: '' }  // Wildcard for 404
+//     {
+//     path: '',
+//     loadComponent: () =>
+//       import('./pages/home/home.component').then((m) => m.HomeComponent)
+//   },
+//   {
+//     path: 'about',
+//     loadComponent: () =>
+//       import('./pages/about/about.component').then((m) => m.AboutComponent)
+//   },
+//   {
+//     path: 'contact',
+//     loadComponent: () =>
+//       import('./pages/contact/contact.component').then((m) => m.ContactComponent)
+//   },
+//   {
+//     path: '**',
+//     redirectTo: ''
+//   }
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', redirectTo: '' }  // Wildcard for 404
 ];
 
 // @NgModule({
--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrl: './contact.component.css'
 })
 export class ContactComponent {
 
