ba02496a5140ab0de9348fa28cf32dfd20175c72
chandrabose27 <102638656+chandrabose27@users.noreply.github.com>
2025-06-09 23:37:58 +0530

    Day 1: Set up routing and created Home, About, Contact pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,37 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+    path: '',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((m) => m.HomeComponent)
+  },
+  {
+    path: 'about',
+    loadComponent: () =>
+      import('./pages/about/about.component').then((m) => m.AboutComponent)
+  },
+  {
+    path: 'contact',
+    loadComponent: () =>
+      import('./pages/contact/contact.component').then((m) => m.ContactComponent)
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+//   { path: '', component: HomeComponent },
+//   { path: 'about', component: AboutComponent },
+//   { path: 'contact', component: ContactComponent },
+//   { path: '**', redirectTo: '' }  // Wildcard for 404
+];
+
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule { }
